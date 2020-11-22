5b052258e96ed65e4ea0cfe4770a60ed0808e902
JakeRSA <jakenudels@gmail.com>
2020-11-22 19:18:39 +0200

    m1-wip, enabled notes to take text from textarea

--- notes/src/App.js
+++ notes/src/App.js
@@ -7,9 +7,9 @@ function App() {
   <div class="container">
     <h1 className="main-header">my notes</h1>
     <MakeNote />
-    <Note description='eat something' />
+    {/* <Note description='eat something' />
     <Note description='shower' />
-    <Note description='broker peace deal between Israelis and Palestinians' /> 
+    <Note description='broker peace deal between Israelis and Palestinians' />  */}
     
   </div>
   )
--- notes/src/components/MakeNote.js
+++ notes/src/components/MakeNote.js
@@ -8,27 +8,35 @@ class NewNote extends React.Component {
         this.notes = [];
         this.state = {
             description: '',
-            dateTime: ''
+            dateTime: '',
+            count: 1
         };
     }
 
+    onDescriptionChange(event) {
+        const value = event.target.value;
+        this.setState({description: value})
+    }
+    
     addNote(description) {
         this.setState({description: description, dateTime: new Date()});
         const note = {
-            description: this.state.description,
-            datetime: this.state.datetime
+            dateTime: this.state.dateTime,
+            description: description,
+            count: this.state.count
         };
         this.notes.push(note);
+        this.setState({count: this.state.count + 1});
     }
 
     render() {
         const noteElems = this.notes.map(note => 
-            <Note key={this.state.dateTime} description={this.state.description} />
+            <Note key={note.count} dateTime={this.state.dateTime} description={note.description}/>
         )
         return (
             <div>
-                <textarea></textarea>
-                <button onClick={() => {this.addNote('hey')}}>
+                <textarea onChange={event => this.onDescriptionChange(event)}></textarea>
+                <button onClick={() => {this.addNote(this.state.description)}}>
                     add
                 </button>
                 <div>{noteElems}</div>
