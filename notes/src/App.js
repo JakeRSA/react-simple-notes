198b5c4f6d3ec194d86d5e7a00d33a0b2568a093
JakeRSA <jakenudels@gmail.com>
2020-11-22 15:08:56 +0200

    m1-wip, added basic new note component

---
+++ notes/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import NewNote from './components/NewNote'
+
+function App() {
+  return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  <div class="container">
+    <h1 className="main-header">my notes</h1>
+    <NewNote />
+  </div>
+  )
+
+}
+
+export default App;
---
+++ notes/src/components/NewNote.js
@@ -0,0 +1,11 @@
+function NewNote(props) {
+    return (
+        <div>
+            <textarea></textarea>
+            <button>add</button>
+        </div>
+
+    )
+}
+
+export default NewNote;
